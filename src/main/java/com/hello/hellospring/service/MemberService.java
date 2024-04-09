b447943c842bdcdd7f271ec4f4364446123e1a7c
leejaewoong <leejaewoong300@gmail.com>
2024-04-09 13:59:24 +0900

    인프런 스프링 강의

---
+++ src/main/java/com/hello/hellospring/service/MemberService.java
@@ -0,0 +1,38 @@
+package com.hello.hellospring.service;
+
+import com.hello.hellospring.domein.Member;
+import com.hello.hellospring.repository.MemberRepository;
+import com.hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+@Transactional
+public class MemberService {
+    private final MemberRepository memberRepository;
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+    public List<Member> findMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+                    throw new IllegalStateException("이미 존재하는 회원입니다.");
+                });
+    }
+}
